236b91a32e724a9eec3939daa70195cdc9d49c0e
Pranay Gaikwad <133897164+dev-pg1021@users.noreply.github.com>
2023-11-20 15:21:12 +0530

    second commit

---
+++ Controllers/ProductListController.cs
@@ -0,0 +1,32 @@
+﻿using Machine_proj.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Machine_proj.Controllers
+{
+    public class ProductListController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Category> list_cat = Category.GetAllCategory();   
+            ProductList model = new ProductList();
+           /* model.CategoryList.Add(new SelectListItem { Text = "Mobiles", Value = "Mb" });
+            model.CategoryList.Add(new SelectListItem { Text = "Shoes", Value = "Sh" });
+            model.CategoryList.Add(new SelectListItem { Text = "Stationary", Value = "St" });
+            model.CategoryList.Add(new SelectListItem { Text = "Books", Value = "Bb" });
+           */
+
+            foreach (var cat in list_cat)
+            {
+                model.CategoryList.Add(new SelectListItem { Text = cat.CategoryName, Value = cat.CategoryCode });
+            }
+            return View(model);
+        }
+
+       /* public IActionResult Index2(string code) {
+            List<Product> prod_lst = ProductList.GetAllProductByCategory(ViewBag.prod_code = code);
+            return View(prod_lst);
+                }
+       */
+    }
+}
---
+++ Models/ProductList.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Machine_proj.Models
+{
+    public class ProductList
+    {
+        public ProductList() {
+            CategoryList = new List<SelectListItem>();
+        }
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public List<SelectListItem> CategoryList { get; set; }
+
+       
+
+        public static List <ProductList> GetAllProduct()
+        {
+            List<ProductList> lstprod = new List<ProductList>();
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=(localdb)\ProjectModels;Initial Catalog=KTjune23;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            con.Open();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Select ProductId.Product, ProductName.Product, CategoryId.Category, CategoryName.Category from Product , Category where CategoryCode.Category = ProductCode.Product" ;
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                   
+
+                    ProductList pl = new ProductList();
+                    pl.ProductId = Convert.ToInt32("ProductId");
+                    pl.CategoryId = Convert.ToInt32("CategoryId");
+                    pl.ProductName = Convert.ToString("ProductName");
+                    pl.CategoryName = Convert.ToString("CategoryName");
+
+
+                    lstprod.Add(pl);
+                }
+                dr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return lstprod;
+        }
+
+
+        public static List<Product> GetAllProductByCategory(String code)
+        {
+            List<Product> lstprod = new List<Product>();
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = @"Data Source=(localdb)\ProjectModels;Initial Catalog=KTjune23;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            con.Open();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "Select * from Product where ProductCode = @code";
+
+                cmd.Parameters.AddWithValue("@code", code);
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+
+
+                    Product pl = new Product();
+                    pl.ProductId = Convert.ToInt32("ProductId");
+                    pl.ProductName = Convert.ToString("ProductName");
+                    lstprod.Add(pl);
+                }
+                dr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return lstprod;
+        }
+
+    }
+}
